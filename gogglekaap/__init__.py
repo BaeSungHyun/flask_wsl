13df4d630e8417cc503f374ff7386778c46f4ba3
baebae <shijr0725@gmail.com>
2023-04-04 07:47:57 +0900

    app hook and app context

--- gogglekaap/__init__.py
+++ gogglekaap/__init__.py
@@ -7,6 +7,7 @@ def create_app():
 
     @app.route("/")
     def index():
+        app.logger.info("RUN HELLO WORLD")
         return "hello world swdwd48"
 
     '''=== Routing Practices ==='''
@@ -40,5 +41,35 @@ def create_app():
     @app.route('/test/urlfor/<path:subpath>')
     def urlfor2(subpath):
         return redirect(url_for('path', subpath=subpath)) # second parameter: parameter for given 'def' 
+
+    ''' === Request hook, Context control1 === '''
+    from flask import g, current_app
+
+    @app.before_first_request  # only at first request
+    def before_first_request():
+        app.logger.info("BEFORE_FIRST_REQUEST")
+    
+    @app.before_request # before business logic on request, this occurs
+    def before_request():
+        g.test = True
+        app.logger.info("BEFORE_REQUEST")
+
+    @app.after_request
+    def after_request(response): # take response as parameter
+        app.logger.info(f"g.test: {g.test}")
+        print("mode: ", current_app.config)
+        print(app.config["DEBUG"])
+        g.test = False
+        app.logger.info("AFTER_REQUEST")
+        return response
+    
+    @app.teardown_request # when request is popped
+    def teardown_request(exception): # take exception(error) as parameter
+        print(g.test)
+        app.logger.info("TEARDOWN_REQEUST")
+
+    @app.teardown_appcontext # when appcontext is popped
+    def teardown_appcontext(exception):
+        app.logger.info("TEARDOWN_APPCONTEXT")
 
     return app
